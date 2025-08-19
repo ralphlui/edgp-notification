7b4b82df6be5800a2151c107d3c3b2792a5a1e66
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-08-20 04:09:01 +0800

    Removed unused import.

--- src/main/java/sg/edu/nus/iss/edgp/notification/controller/EmailNotificationController.java
+++ src/main/java/sg/edu/nus/iss/edgp/notification/controller/EmailNotificationController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
